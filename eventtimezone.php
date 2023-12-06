39a569673a57385c5ce1a7f21f8ad4cb3261d436
vinay-osseed <vinay@osseed.com>
2023-12-06 13:13:44 +0530

    feat: new upgraded extension.

--- eventtimezone.php
+++ eventtimezone.php
@@ -13,15 +13,6 @@ function eventtimezone_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function eventtimezone_civicrm_xmlMenu(&$files) {
-  _eventtimezone_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -73,54 +64,6 @@ function eventtimezone_civicrm_disable() {
  */
 function eventtimezone_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _eventtimezone_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function eventtimezone_civicrm_managed(&$entities) {
-  _eventtimezone_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function eventtimezone_civicrm_caseTypes(&$caseTypes) {
-  _eventtimezone_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function eventtimezone_civicrm_angularModules(&$angularModules) {
-  _eventtimezone_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function eventtimezone_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _eventtimezone_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -363,3 +306,12 @@ function eventtimezone_civicrm_tokenValues(&$values, &$cids, $job = null, $token
     }
   }
 }
+
+// /**
+//  * Implements hook_civicrm_entityTypes().
+//  *
+//  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+//  */
+// function eventtimezone_civicrm_entityTypes(&$entityTypes) {
+//   _eventtimezone_civix_civicrm_entityTypes($entityTypes);
+// }
